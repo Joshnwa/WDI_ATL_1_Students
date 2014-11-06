ab66b86bb23cd3d03dcfa99df8b2487cf7b4ee38
Michael Hopper <mike.hopper@ga.co>
2014-11-06 14:03:00 -0500

    Updates to jshint starter.

--- 07week/jshint_starter/starter.js
+++ 07week/jshint_starter/starter.js
@@ -1,18 +1,16 @@
-'use strict';
-
 function sum(a, b) {
   result = a + b
   return result;
 }
 
 function printGreeting(greeting, name) {
-  if (greeting == null) {
-    greeting = "Hello"
+  if (greeting == null || greeting === undefined) {
+      greeting = "Hello";
   }
-  if (name == null) {
+if (name === null || name == undefined) {
     name = 'World';
   }
-  console.log(greeting, ' ', name);
+  console.log(greeting + ' ' + name);
 }
 
 console.log('sum(3, 4) = ' + sum(3, 4));
