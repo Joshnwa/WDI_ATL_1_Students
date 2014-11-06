6ca635d94e124aeb7409413901cc2a01b7fb7885
Michael Hopper <mike.hopper@ga.co>
2014-11-06 13:25:23 -0500

    Added jshint starter

---
+++ 07week/jshint_starter/starter.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function sum(a, b) {
+  result = a + b
+  return result;
+}
+
+function printGreeting(greeting, name) {
+  if (greeting == null) {
+    greeting = "Hello"
+  }
+  if (name == null) {
+    name = 'World';
+  }
+  console.log(greeting, ' ', name);
+}
+
+console.log('sum(3, 4) = ' + sum(3, 4));
+
+printGreeting("Hello", 'John');
+printGreeting(null, 'Susan');
+printGreeting();
