d561db8de659a7fced39541fb566626db42e4610
marcwright <marc.wright@gmail.com>
2014-11-12 09:58:54 -0500

    changed this to  and took out market in html in stalls-angular app

--- 08week/stalls_angular/js/app.js
+++ 08week/stalls_angular/js/app.js
@@ -1,13 +1,13 @@
 (function() {
     var app = angular.module('leatherLaneMarketApp', []);
 
-     app.controller('MarketController', function($scope){
+     app.controller('MarketController', ["$scope", function($scope){
        $scope.basket = 0;
 
        $scope.addToBasket = function() {
        $scope.basket ++
        }
-     });
+     }]);
 
 
 
