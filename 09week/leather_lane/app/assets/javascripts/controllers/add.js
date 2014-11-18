ecfb7bc0eb18e61a3afd16fbae0ad05d67be0fca
Michael Hopper <mike.hopper@ga.co>
2014-11-18 10:20:33 -0500

    Fixed the animation.

--- 09week/leather_lane/app/assets/javascripts/controllers/add.js
+++ 09week/leather_lane/app/assets/javascripts/controllers/add.js
@@ -7,16 +7,24 @@ angular.module('leatherLaneMarketApp')
 
   $scope.stalls = stallService.stalls;
 
+  var reset = function() {
+    $scope.newStall = null;
+    $scope.stallForm.$setPristine();
+    $location.url('/market');
+  };
+
   $scope.addStall = function() {
-    stallService.add($scope.newStall)
-    .success(function(data) {
+    stallService.add($scope.newStall).success(function(data) {
       $scope.stalls.push(data);
-      $scope.newStall = null;
-      $scope.stallForm.$setPristine();
-      $location.url('/#/market');
-    }).error(function() {
+      reset();
+    })
+    .error(function() {
       alert('Something went wrong!');
     });
+  };
+
+  $scope.cancel = function() {
+    reset();
   };
 
 }]);
